0f1d4618a5116aa919e96bfa9c60cfddaea28b57
daniel hernandez <danielalejandro627@gmail.com>
2024-08-27 18:28:16 -0500

    Primer commit

---
+++ AppDos/Arquitectura/app/src/main/java/com/example/appdos/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.appdos;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mensaje1;
+    EditText txt1,txt2,txt3;
+    Button btnc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mensaje1 = (TextView) findViewById(R.id.mensaje1);
+        txt1 = (EditText) findViewById(R.id.txt1);
+        txt2 = (EditText) findViewById(R.id.txt2);
+        txt3 = (EditText) findViewById(R.id.txt3);
+        btnc = (Button) findViewById(R.id.btnc);
+    }
+
+    public void calculo(View view){
+
+    }
+}
