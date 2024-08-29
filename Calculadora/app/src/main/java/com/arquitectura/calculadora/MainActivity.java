10ddc0baaf51ddd87fc9b4bcd77d7fbad71281f7
daniel hernandez <danielalejandro627@gmail.com>
2024-08-29 18:28:26 -0500

    Declaracion de variables

--- Calculadora/app/src/main/java/com/arquitectura/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/arquitectura/calculadora/MainActivity.java
@@ -1,6 +1,9 @@
 package com.arquitectura.calculadora;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txt_n1,txt_n2;
+    TextView txt_result;
+    RadioButton rbtn_suma,rbtn_resta,rbtn_multi,rbtn_divi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txt_n1 = (EditText) findViewById(R.id.txt_n1);
+        txt_n2 = (EditText) findViewById(R.id.txt_n2);
+        txt_result = (TextView) findViewById(R.id.txt_result);
+        rbtn_suma = (RadioButton) findViewById(R.id.rbtn_suma);
+        rbtn_resta = (RadioButton) findViewById(R.id.rbtn_resta);
+        rbtn_multi = (RadioButton) findViewById(R.id.rbtn_multi);
+        rbtn_divi = (RadioButton) findViewById(R.id.rbtn_divi);
     }
 }
